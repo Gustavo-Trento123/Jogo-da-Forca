da02a0a886dca270a8786224f3d45289554f5aec
Gustavo <gustavo.trento@etec.sp.gob.br>
2024-12-02 22:23:49 -0300

    Alteração do desenho dos underlines
    I changed the way the program draws the underlines, so that i can use an array to better manipulate the letters.
    
    i also added comments to make the code easier to understand.

--- src/engine/script.js
+++ src/engine/script.js
@@ -1,19 +1,40 @@
 function atribuirUnderLine(tamanho_palavra){
+    let id = 0;
 
-    for(let i = 0; i <= tamanho_palavra; i++){
+    for(let j = 0; j<= tamanho_palavra; j++){
+        letras_advinhadas[j] ='_';
+        console.log(letras_advinhadas[j]); 
+    }
+
+    letras_advinhadas.forEach(letra => {
         let ul = document.querySelector('ul');
         let li = document.createElement('li');
-        li.textContent = "_";
+
+        li.textContent = letra;
+        li.id = id;
         ul.appendChild(li);
-    }
+
+        id++;
+
+    })
 
 }
 
 function main(){
+
+    /* Biblioteca de palavras que serão usadas no jogo */
     let biblioteca_palavras = ["SOL", "ESCOLA", "ORELHA", "ACOMPANHAR", "ESTUDO", "FACIAL", "REDES", "AMERICANO"];
+
+    /* Sortea uma palavra da biblioteca de palavras para começar o jogo */
     let palavra = biblioteca_palavras[Math.floor(Math.random() *  7)];
+
+    /* Recebe o tamanho da palavra para começar a colocar a quantidade certa de underlines */
     let tamanho_palavra = palavra.length;
-    
+
+    /* nesta array ficará as letras acertadas */
+    letras_advinhadas = [];
+
+    /* Chama a função que desenha a forca */
     atribuirUnderLine(tamanho_palavra - 1);
     console.log(palavra);
 }
